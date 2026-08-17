f048787ee7283cb99f329a9adb30e0216e2467f6
ybaronas <ybaronas@gmail.com>
2026-08-17 13:56:58 +0000

    Primeira alteração Rest Controller e Olá Mundo

--- src/main/java/com/aula_tads/primeira_api/PrimeiraApiApplication.java
+++ src/main/java/com/aula_tads/primeira_api/PrimeiraApiApplication.java
@@ -2,12 +2,26 @@ package com.aula_tads.primeira_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+
+
 public class PrimeiraApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiraApiApplication.class, args);
 	}
+
+
+	@GetMapping("/")
+	public String hello() {
+		return "Ola Mundo!" ;
+}
+
+
+
 
 }
