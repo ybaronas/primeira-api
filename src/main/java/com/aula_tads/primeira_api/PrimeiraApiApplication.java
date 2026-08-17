65545270e79c3e378095e438a356660c083db60b
ybaronas <ybaronas@gmail.com>
2026-08-17 10:20:25 -0300

    Add files via upload

---
+++ src/main/java/com/aula_tads/primeira_api/PrimeiraApiApplication.java
@@ -0,0 +1,13 @@
+package com.aula_tads.primeira_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aula_tads/primeira_api/PrimeiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula_tads.primeira_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
